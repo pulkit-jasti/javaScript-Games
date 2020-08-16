4bf8083a8e200dfc1e27f10a057d4dd9070b9396
pulkit-jasti <31801730+pulkit-jasti@users.noreply.github.com>
2020-08-16 20:33:18 +0530

    create element function

---
+++ Memory Game/script.js
@@ -0,0 +1,173 @@
+const cardArray = [
+    {
+        name: 'burger',
+        img: 'images/burger.png'
+    },
+    {
+        name: 'doughnut',
+        img: 'images/doughnut.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'ice-cream-2',
+        img: 'images/ice-cream-2.png'
+    },
+    {
+        name: 'ice-cream-3',
+        img: 'images/ice-cream-3.png'
+    },
+    {
+        name: 'ketchup',
+        img: 'images/ketchup.png'
+    },
+    {
+        name: 'pineapple',
+        img: 'images/pineapple.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+    {
+        name: 'strawberry',
+        img: 'images/strawberry.png'
+    },
+    {
+        name: 'watermelon',
+        img: 'images/watermelon.png'
+    },
+];
+
+
+
+var obj = document.getElementById('card-container')
+console.log(obj);
+obj.style.backgroundColor='yellow'
+
+var obj2 = document.getElementsByClassName('grid')
+console.log(obj2);
+function myFunction() {
+    let index = 0;
+    const numberOfCards = 20
+    for(let i=0;i<=numberOfCards;i++){
+        const max = 9;
+        var btn = document.createElement("img");
+        btn.src = cardArray[index].img;
+        document.getElementById('grid').appendChild(btn);
+        index++;
+        if(index>9){index=0};
+    }
+}
+
+myFunction();
+
+//randomFunction();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*document.addEventListener('DOMContentLoaded',() => {
+    const cardArray = [
+        {
+            name: 'burger',
+            img: 'images/burger.png'
+        },
+        {
+            name: 'doughnut',
+            img: 'images/doughnut.png'
+        },
+        {
+            name: 'ice-cream-1',
+            img: 'images/ice-cream-1.png'
+        },
+        {
+            name: 'ice-cream-2',
+            img: 'images/ice-cream-2.png'
+        },
+        {
+            name: 'ice-cream-3',
+            img: 'images/ice-cream-3.png'
+        },
+        {
+            name: 'ketchup',
+            img: 'images/ketchup.png'
+        },
+        {
+            name: 'pineapple',
+            img: 'images/pineapple.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'strawberry',
+            img: 'images/strawberry.png'
+        },
+        {
+            name: 'watermelon',
+            img: 'images/watermelon.png'
+        },
+    ];
+
+    const grid = document.querySelector('.grid');
+
+    
+
+
+    //var object = document.getElementsByClassName('card-container');
+
+
+
+    for(let i=0;i<5;i++){
+        console.log('function is running '+i);
+    }
+
+})
+*/
