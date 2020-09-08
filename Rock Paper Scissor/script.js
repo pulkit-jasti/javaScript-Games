6ca6ccacc1d1c3b6e192c6983ad1cd6a0d470ff2
pulkit-jasti <31801730+pulkit-jasti@users.noreply.github.com>
2020-09-08 20:48:16 +0530

    styling update

--- Rock Paper Scissor/script.js
+++ Rock Paper Scissor/script.js
@@ -1,14 +1,14 @@
 let hands = [
 	{
-		hand: 'rock',
+		hand: 'Rock',
 		address: 'images/rock.png',
 	},
 	{
-		hand: 'paper',
+		hand: 'Paper',
 		address: 'images/paper.png',
 	},
 	{
-		hand: 'scissor',
+		hand: 'Scissor',
 		address: 'images/scissors.png',
 	},
 ];
@@ -20,20 +20,45 @@ let scissor = document.getElementById('scissor');
 let player = document.getElementById('player-hand');
 let computer = document.getElementById('computer-hand');
 
+let displayPlayerScore = document.getElementById('player-score');
+let displayComputerScore = document.getElementById('computer-score');
+
+let playerScore = 0;
+let computerScore = 0;
+
 enableClicks();
 
 function playGame() {
-	if (this.innerHTML === 'Rock') {
-		player.src = hands[0].address;
-	} else if (this.innerHTML === 'Paper') {
-		player.src = hands[1].address;
-	} else if (this.innerHTML === 'Scissor') {
-		player.src = hands[2].address;
-	}
 	disableClicks();
 
 	let computerChoice = Math.floor(Math.random() * 3);
 	computer.src = hands[computerChoice].address;
+
+	if (this.innerHTML === 'Rock') {
+		player.src = hands[0].address;
+		if (computerChoice == 2) {
+			playerScore++;
+		} else if (computerChoice == 1) {
+			computerScore++;
+		}
+	} else if (this.innerHTML === 'Paper') {
+		player.src = hands[1].address;
+		if (computerChoice == 0) {
+			playerScore++;
+		} else if (computerChoice == 2) {
+			computerScore++;
+		}
+	} else if (this.innerHTML === 'Scissor') {
+		player.src = hands[2].address;
+		if (computerChoice == 0) {
+			computerScore++;
+		} else if (computerChoice == 1) {
+			playerScore++;
+		}
+	}
+
+	displayPlayerScore.innerHTML = `Player: ${playerScore}`;
+	displayComputerScore.innerHTML = `Computer: ${computerScore}`;
 
 	setTimeout(() => {
 		player.src = hands[0].address;
