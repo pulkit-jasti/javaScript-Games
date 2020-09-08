03900e60ba33a366a03babc7cbe4f860c2116411
pulkit-jasti <31801730+pulkit-jasti@users.noreply.github.com>
2020-09-08 21:50:32 +0530

    hand shaking animation

--- Rock Paper Scissor/script.js
+++ Rock Paper Scissor/script.js
@@ -31,40 +31,48 @@ enableClicks();
 function playGame() {
 	disableClicks();
 
+	player.style.animation = 'shakePlayer 2s ease';
+	computer.style.animation = 'shakeComputer 2s ease';
+
 	let computerChoice = Math.floor(Math.random() * 3);
-	computer.src = hands[computerChoice].address;
-
-	if (this.innerHTML === 'Rock') {
-		player.src = hands[0].address;
-		if (computerChoice == 2) {
-			playerScore++;
-		} else if (computerChoice == 1) {
-			computerScore++;
-		}
-	} else if (this.innerHTML === 'Paper') {
-		player.src = hands[1].address;
-		if (computerChoice == 0) {
-			playerScore++;
-		} else if (computerChoice == 2) {
-			computerScore++;
-		}
-	} else if (this.innerHTML === 'Scissor') {
-		player.src = hands[2].address;
-		if (computerChoice == 0) {
-			computerScore++;
-		} else if (computerChoice == 1) {
-			playerScore++;
-		}
-	}
-
-	displayPlayerScore.innerHTML = `Player: ${playerScore}`;
-	displayComputerScore.innerHTML = `Computer: ${computerScore}`;
-
 	setTimeout(() => {
-		player.src = hands[0].address;
-		computer.src = hands[0].address;
-		enableClicks();
-	}, 4000);
+		computer.src = hands[computerChoice].address;
+
+		if (this.innerHTML === 'Rock') {
+			player.src = hands[0].address;
+			if (computerChoice == 2) {
+				playerScore++;
+			} else if (computerChoice == 1) {
+				computerScore++;
+			}
+		} else if (this.innerHTML === 'Paper') {
+			player.src = hands[1].address;
+			if (computerChoice == 0) {
+				playerScore++;
+			} else if (computerChoice == 2) {
+				computerScore++;
+			}
+		} else if (this.innerHTML === 'Scissor') {
+			player.src = hands[2].address;
+			if (computerChoice == 0) {
+				computerScore++;
+			} else if (computerChoice == 1) {
+				playerScore++;
+			}
+		}
+
+		displayPlayerScore.innerHTML = `Player: ${playerScore}`;
+		displayComputerScore.innerHTML = `Computer: ${computerScore}`;
+
+		player.style.animation = '';
+		computer.style.animation = '';
+
+		setTimeout(() => {
+			player.src = hands[0].address;
+			computer.src = hands[0].address;
+			enableClicks();
+		}, 4000);
+	}, 2000);
 }
 
 function disableClicks() {
